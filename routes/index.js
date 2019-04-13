fc08ee95eb0bc5aae0950b0da96504cf80f6a39e
ashish005 <me.ashish005@gmail.com>
2019-04-13 14:12:26 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,24 @@
 
         const viewRouter = (customData, req, res, next)=> {
             res.render(customData.path, { title: customData.title });
-        }
+        };
+
+        const isAuthenticated = (req, res, next)=> {
+            // do any checks you want to in here
+
+            // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
+            // you can do this however you want with whatever variables you set up
+            if (req.user && req.user.authenticated)
+                return next();
+
+            // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
+            res.redirect('/');
+        };
 
         router.get('/', viewRouter.bind(null, { path:'index', title: 'Default' }));
         router.get('/login', viewRouter.bind(null, { path:'login', title: 'Login' }));
         router.get('/signup', viewRouter.bind(null, { path:'signup', title: 'signup' }));
-        router.get('/users', viewRouter.bind(null, { path:'users', title: 'users' }));
+        router.get('/users', isAuthenticated, viewRouter.bind(null, { path:'users', title: 'users' }));
 
         return router;
     }
